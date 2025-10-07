6194fe1567f0a0cdda211f690fbc6e232e33935c
jason13official <jason13official@gmail.com>
2025-10-07 00:41:00 -0400

    fix: bump mod version (for license inclusion)

--- common/src/main/java/com/cursee/monolib/Constants.java
+++ common/src/main/java/com/cursee/monolib/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
   public static final String MOD_ID = "monolib";
   public static final String MOD_NAME = "MonoLib";
-  public static final String MOD_VERSION = "3.1.0";
+  public static final String MOD_VERSION = "3.1.1";
   public static final String MOD_PUBLISHER = "Lupin";
   public static final String MOD_URL = "https://www.curseforge.com/minecraft/mc-mods/monolib";
   public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
